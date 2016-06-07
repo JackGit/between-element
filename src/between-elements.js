152bd8c1c6861907794f6e64f57f59c71c27df26
jack <ygjack414@hotmail.com>
2016-06-07 11:52:56 +0800

    add addClass / removeClass

--- src/between-elements.js
+++ src/between-elements.js
@@ -19,157 +19,241 @@
         //'transform', 'transformOrigin' // translate will be handled by BCR, but scale/skew/rotate can't be handled
     ];
 
-    function BetweenElements() {
+    function BetweenElements(options) {
+        var defaultOptions = {
+            easing: 'ease',
+            duration: 300,   // ms
+            removeFromElement: false
+        };
+
+        this.options = Object.assign(defaultOptions, options);
+
         this.fromElement = null;
-        this.fromBCR = null;
         this.fromStyle = null;
+        this.fromBCR = null;
+        this.clonedElement = null;
+        this.toElements = []; // {el, BRC, style}
 
-        this.toElement = null;
-        this.toBCR = null;
-        this.toStyle = null;
-
-        this.transitionEnd = false;
+        this.status = ''; // '' -> 'transitioning' -> 'end'
+        this.transitionEndHandler = this._end.bind(this);
+        this.eventHandlers = {};
     }
-
 
     BetweenElements.prototype = {
 
         constructor: BetweenElements,
 
+        _transitionValue: function(duration, easing) {
+            return 'all ' + duration + 'ms ' + easing;
+        },
+
         _clone: function() {
-            var cloneElement;
+            var toElement = this.toElements[0];
 
             // if there is only a text node child, then do deep clone
             if(this.fromElement.childNodes.length === 1 && this.fromElement.childNodes[0].nodeType === 3)
-                cloneElement = this.fromElement.cloneNode(true);
+                this.clonedElement = this.fromElement.cloneNode(true);
             else
-                cloneElement = this.fromElement.cloneNode();
+                this.clonedElement = this.fromElement.cloneNode();
 
             // position the cloned element
-            cloneElement.style.position = 'fixed';
-            cloneElement.style.top = this.fromBCR.top + 'px';
-            cloneElement.style.left = this.fromBCR.left + 'px';
-            //cloneElement.style.width = this.fromBCR.width + 'px';
-            //cloneElement.style.height = this.fromBCR.height + 'px';
-            cloneElement.style.transition = 'all 3s ease';
+            this.clonedElement.style.position = 'fixed';
+            this.clonedElement.style.top = this.fromBCR.top + 'px';
+            this.clonedElement.style.left = this.fromBCR.left + 'px';
+            this.clonedElement.style.transition = this._transitionValue(toElement.options.duration, toElement.options.easing);
 
             // need to set margin as 0, coz getBoundingClientRect doesn't contain margin
-            cloneElement.style.margin = '0';
+            this.clonedElement.style.margin = '0';
 
             // remove the origin from element
-            // this.fromElement.parentElement.removeChild(this.fromElement); // remove will coz reflow of element in the same page
-            this.fromElement.style.opacity = 0;
+            if(toElement.options.removeFromElement || toElement.className) {
+                this.fromElement.style.transition = 'none';
+                this.fromElement.style.opacity = 0;
+                this.fromElement.style.pointerEvents = 'none';
+            }
 
-            // set new from element
-            this.fromElement = cloneElement;
-            document.body.appendChild(cloneElement);
+            document.body.appendChild(this.clonedElement);
         },
 
         _transition: function() {
-            this.toStyle.position = 'fixed';
-            this.toStyle.top = this.toBCR.top + 'px';
-            this.toStyle.left = this.toBCR.left + 'px';
-            this.toStyle.transition = 'all 3s ease';
-            this.toStyle.margin
-                = this.toStyle.marginLeft
-                = this.toStyle.marginRight
-                = this.toStyle.marginTop
-                = this.toStyle.marginBottom
+            var that = this;
+            var toStyle = this.toElements[0].style;
+            var toBCR = this.toElements[0].BCR;
+
+            toStyle.position = 'fixed';
+            toStyle.top = toBCR.top + 'px';
+            toStyle.left = toBCR.left + 'px';
+            toStyle.margin
+                = toStyle.marginLeft
+                = toStyle.marginRight
+                = toStyle.marginTop
+                = toStyle.marginBottom
                 = 0;
 
-            var _end = function() {
-                if(this.transitionEnd) {
-                    this.fromElement.removeEventListener('transitionend', _end);
-                    return;
-                }
-
-                console.log('transition end');
-                this.transitionEnd = true;
-                this.fromElement.remove();
-            }.bind(this);
-
-            this.fromElement.addEventListener('transitionend', _end);
-            this.transitionEnd = false;
+            this.clonedElement.addEventListener('transitionend', this.transitionEndHandler);
 
             requestAnimationFrame(function() {
-                var style = getComputedStyle(this.fromElement);
+                var style = getComputedStyle(that.clonedElement);
 
                 TRANSITION_PROPERTIES.forEach(function(name) {
-                    if(style[name] != this.toStyle[name])
-                        this.fromElement.style[name] = this.toStyle[name];
-                }, this);
-            }.bind(this));
-
+                    if(style[name] != toStyle[name])
+                        that.clonedElement.style[name] = toStyle[name];
+                });
+            });
         },
 
-        start: function() {
-            if(!this.fromElement || !this.toElement) {
+        _end: function() {
+            if(this.status === 'end') {
+                this.clonedElement.removeEventListener('transitionend', this.transitionEndHandler);
+                this.clonedElement = null;
+                this._trigger('complete');
+
+                // consume next toElement from the array
+                if(this.toElements.length > 0)
+                    requestAnimationFrame(this._start.bind(this));
+                else
+                    this._trigger('allComplete');
+
+                return;
+            }
+
+            var from = this.toElements.shift();
+
+            this.status = 'end';
+            this.fromElement = from.el;
+            this.fromBCR = from.BCR;
+            this.fromStyle = from.style;
+            this.clonedElement.remove();
+
+            if(from.className) {
+                from.removeClass ?
+                    this.fromElement.classList.remove(from.className) :
+                    this.fromElement.classList.add(from.className);
+
+                this.fromElement.style.transition = this.fromStyle.transition;
+                this.fromElement.style.opacity = this.fromStyle.opacity;
+                this.fromElement.style.pointerEvents = this.fromStyle.pointerEvents;
+            }
+        },
+
+        _trigger: function(event) {
+            (this.eventHandlers[event] || []).forEach(function(callback) {
+                if(typeof callback === 'function')
+                    callback();
+            });
+        },
+
+        _start: function() {
+            if(!this.fromElement || this.toElements.length === 0) {
                 console.error('[BetweenElement] please set from and to element');
                 return;
             }
 
+            this.status = 'transitioning';
             this._clone();
-            // this.toElement.style.opacity = 0;
             this._transition();
-        },
-
-        stop: function() {
-
-        },
-
-        cancel: function() {
-            this.fromElement.remove();
-        },
-
-        rollback: function() {
-
         },
 
         from: function(el) {
             this.fromElement = el;
             this.fromBCR = el.getBoundingClientRect();
-            this.fromStyle = Object.assign({}, getComputedStyle(el));
+            this.fromStyle = Object.assign({}, getComputedStyle(this.fromElement));
             return this;
         },
 
-        to: function(el) {
-            this.toElement = el;
-            this.toBCR = el.getBoundingClientRect();
-            this.toStyle = Object.assign({}, getComputedStyle(el));
+        to: function(el, options) {
+            if(this.fromElement === el)
+                return;
+
+            var style;
+
+            options = Object.assign({}, this.options, options);
+
+            // el is another element
+            style = Object.assign({}, getComputedStyle(el));
+            this.toElements.push({
+                el: el,
+                BCR: el.getBoundingClientRect(),
+                style: style,
+                options: options
+            });
+
+            if(this.status !== 'transitioning')
+                this._start();
+
+            return this;
+        },
+
+        addClass: function(className, options) {
+            if(this.fromElement.classList.contains(className))
+                return;
+
+            var style;
+
+            options = Object.assign({}, this.options, options);
+            this.fromElement.classList.add(className);
+
+            style = Object.assign({}, getComputedStyle(this.fromElement));
+            style.opacity = this.fromStyle.opacity;
+            style.transition = this.fromStyle.transition;
+            style.pointerEvents = this.fromStyle.pointerEvents;
+
+            this.toElements.push({
+                el: this.fromElement,
+                className: className,
+                removeClass: false,
+                BCR: this.fromElement.getBoundingClientRect(),
+                style: style,
+                options: options
+            });
+
+            this.fromElement.classList.remove(className);
+
+            if(this.status !== 'transitioning')
+                this._start();
+
+            return this;
+        },
+
+        removeClass: function(className, options) {
+            if(!this.fromElement.classList.contains(className))
+                return;
+
+            var style;
+
+            options = Object.assign({}, this.options, options);
+            this.fromElement.classList.remove(className);
+
+            style = Object.assign({}, getComputedStyle(this.fromElement));
+            style.opacity = this.fromStyle.opacity;
+            style.transition = this.fromStyle.transition;
+            style.pointerEvents = this.fromStyle.pointerEvents;
+
+            this.toElements.push({
+                el: this.fromElement,
+                className: className,
+                removeClass: true,
+                BCR: this.fromElement.getBoundingClientRect(),
+                style: style,
+                options: options
+            });
+
+            this.fromElement.classList.add(className);
+
+            if(this.status !== 'transitioning')
+                this._start();
+
+            return this;
+        },
+
+        on: function(event, callback) {
+            if(!this.eventHandlers[event])
+                this.eventHandlers[event] = [];
+
+            this.eventHandlers[event].push(callback);
             return this;
         }
     };
-
-    function clone(e) {
-        var cloneElement;
-
-        // if there is only a text node child, then do deep clone
-        if(e.childNodes.length === 1 && e.childNodes[0].nodeType === 3)
-            cloneElement = e.cloneNode(true);
-        else
-            cloneElement = e.cloneNode();
-
-        // need to set margin as 0, coz getBoundingClientRect doesn't contain margin
-        cloneElement.style.margin = '0';
-
-        return cloneElement;
-    }
-
-    function drawBoundingClientRect(e) {
-        var rect = e.getBoundingClientRect();
-        var overlay = document.createElement('div');
-
-        overlay.style.position = 'fixed';
-        overlay.style.top = rect.top + 'px';
-        overlay.style.left = rect.left + 'px';
-        overlay.style.width = rect.width + 'px';
-        overlay.style.height = rect.height + 'px';
-        overlay.style.backgroundColor = 'black';
-        overlay.style.opacity = 0.4;
-
-        document.body.appendChild(overlay);
-
-    }
 
     return BetweenElements;
 }));
